71f93e101d9b224481956d04ab1cef2f4b61fe41
Kevin Donnelly <mitchell@upstairs-imac.com>
2020-07-28 11:27:46 -0400

    Removed argparse in run.py; updated exit

--- run.py
+++ run.py
@@ -5,24 +5,21 @@ usage: python3.8 run.py [-h]
 
 optional arguments:
   -h, --help  show this help message and exit
-
 """
 
 import sys
-from argparse import ArgumentParser
 
 from PyQt5.QtWidgets import QApplication
 
 from source import bar_chart
 
 if __name__ == "__main__":
-    parser = ArgumentParser(
-        prog="python3.8 run.py",
-        description="Show an animation of the Huntington–Hill apportionment method")
-
-    args = parser.parse_args()
+    if "-h" in sys.argv or "--help" in sys.argv:
+        print(__doc__)
+        exit()
 
     app = QApplication(sys.argv)
 
     ex = bar_chart.App()
-    sys.exit(app.exec_())
+
+    exit(app.exec_())
